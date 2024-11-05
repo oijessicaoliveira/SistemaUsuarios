fc41edcff5f05e5ce4dff16ac55cad6491ff13b9
Jessica Oliveira <eijessicasousa@gmail.com>
2024-11-04 21:22:53 -0300

    Atualizada - 04/11/2024.

--- teste.php
+++ teste.php
@@ -9,8 +9,10 @@ $editor = new FileEditor();
 
 // Adiciona um novo usuário ao arquivo, chamando o método writeUser da classe FileEditor com um array que representa o usuário
 // Este método cria o arquivo "usuarios.txt" caso ele ainda não exista, ou adiciona os dados ao final do arquivo, caso ele já exista
-$editor->writeUser(['1', 'Ana Silva', 'ana@email.com']);
-$editor->writeUser(['2', 'Carlos Souza', 'carlos@email.com']);
+$editor->writeUser(['1', 'Danilo Melo Silva', 'danilo@email.com']);
+$editor->writeUser(['2', 'Carla Costa Azevedo', 'carla@email.com']);
+$editor->writeUser(['3', 'Vinicius Ribeiro Castro', 'vinicius@email.com']);
+$editor->writeUser(['4', 'Leila Pinto Araujo', 'leila@email.com']);
 
 // Lê todos os usuários do arquivo usando o método readUsers da classe FileEditor e armazena-os na variável $usuarios
 $usuarios = $editor->readUsers();
